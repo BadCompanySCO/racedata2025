77b0d3ca5edfd8980c97d5cfd8152d2032b3ad3b
BadCompanySCO <145200812+BadCompanySCO@users.noreply.github.com>
2025-01-04 23:03:49 +0000

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,32 @@
 import streamlit as st
+import pandas as pd
+import openpyxl as opxl
+import plotly.express as px  # pip install plotly-express
 
-st.title("🎈 My new app")
+
+
+st.title("🎈 We Run Scotland ")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Upload Race Data"
 )
+
+# File uploader widget
+uploaded_file = st.file_uploader("Choose an XLSX file", type=["xlsx"])
+
+if uploaded_file is not None:
+    # Read the Excel file into a pandas DataFrame
+    try:
+        df = pd.read_excel(uploaded_file)
+    except Exception as e:
+        st.error(f"Error reading the file: {e}")
+        df = None
+
+    if df is not None:
+        # Display the DataFrame
+        st.dataframe(df)
+
+        # Optional: Add more functionalities here
+        # For example:
+        # - Display summary statistics
+        # - Allow users to filter or sort the data
+        # - Create visualizations
